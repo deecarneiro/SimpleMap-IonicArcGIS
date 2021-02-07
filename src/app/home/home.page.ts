4a999af237f9e9934e2d5e12d816e7b16af256d2
danita240697 <deysedayane.o.c@gmail.com>
2021-02-07 19:10:05 -0300

    Integrating ArcGIS Map with Ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Map from '@arcgis/core/Map';
+import MapView from '@arcgis/core/views/MapView';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   constructor() {}
+  ngOnInit(): void {
+
+    const map = new Map({
+      basemap: "topo-vector" //Reference to the base of the map
+    });
+
+    const view = new MapView({
+      container: "container", // Reference to the view div 
+      map: map, // Reference to the map object created before the view
+      zoom: 4, // Sets zoom level based on level of detail (LOD)
+      center: [15, 65] // Sets center point of view using longitude,latitude
+    });
+
+  }
 
 }
